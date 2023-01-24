c73484266223f75352ff501c7c92b29d30859065
sbcshop2 <120171682+sbcshop2@users.noreply.github.com>
2023-01-24 12:23:14 +0530

    Update Display_images.ino

--- Examples/Display_images/Display_images/Display_images/Display_images.ino
+++ Examples/Display_images/Display_images/Display_images/Display_images.ino
@@ -1,9 +1,9 @@
 
 #include <TFT_eSPI.h>
-#include "Not_all_heros_wear_capes_lettering.h" //Image 1
-#include "Metaverse.h"                          //Image 2
-#include "Flower_pot_with_flowers.h"            //Image 3
-#include "sb_logo.h"            //Image 4
+#include "sb_logo.h"                 //Image 1
+#include "SB_image.h"                 //Image 2
+#include "Flower_pot_with_flowers.h"  //Image 3   
+
 
 TFT_eSPI ipsdisp= TFT_eSPI();           
 
@@ -17,25 +17,24 @@ void setup() {
 }
 
 void loop() {
- ipsdisp.pushImage(0,0,240,320,image1);    //ipsdisp.pushImage(x1,y1,x2,y2,array of image 1 containting 16-bit RGB565 data of each pixel) 
- ipsdisp.setCursor(0,295,4);               //ipsdisp.setCurser(x axis,y axis,text font style i.e 1/2/4/6)
- ipsdisp.setTextColor(TFT_CYAN,TFT_BLACK); //ipsdisp.setTextColor(text color,text background color)
- ipsdisp.println(F("IMG TYP:Calligraphy  "));
- delay(10000);
- ipsdisp.pushImage(0,0,240,320,image2);    //ipsdisp.pushImage(x1,y1,x2,y2,array of image 2 containting 16-bit RGB565 data of each pixel) 
- ipsdisp.setCursor(0, 295,4);              //ipsdisp.setCurser(x axis,y axis,text font style i.e 1/2/4/6)
- ipsdisp.setTextColor(TFT_CYAN,TFT_BLACK); //ipsdisp.setTextColor(text color,text background color)
- ipsdisp.println(F("IMG TYP:Animated     "));
- delay(10000); 
- ipsdisp.pushImage(0,0,240,320,image3);    //ipsdisp.pushImage(x1,y1,x2,y2,array of image 3 containting 16-bit RGB565 data of each pixel) 
- ipsdisp.setCursor(0,295,4);               //ipsdisp.setCurser(x axis,y axis,text font style i.e 1/2/4/6)
- ipsdisp.setTextColor(TFT_CYAN,TFT_BLACK); //ipsdisp.setTextColor(text color,text background color)
- ipsdisp.println(F("IMG TYP:HD Photo     "));
- delay(10000);  
+
 
   ipsdisp.pushImage(0,0,240,240,image4sb_logo);    //ipsdisp.pushImage(x1,y1,x2,y2,array of image 3 containting 16-bit RGB565 data of each pixel) 
  ipsdisp.setCursor(0,295,4);               //ipsdisp.setCurser(x axis,y axis,text font style i.e 1/2/4/6)
  ipsdisp.setTextColor(TFT_CYAN,TFT_BLACK); //ipsdisp.setTextColor(text color,text background color)
  ipsdisp.println(F("IMG TYP:HD Photo     "));
- delay(10000); 
+ delay(1000); 
+
+ ipsdisp.pushImage(0,0,240,320,SB_image2);    //ipsdisp.pushImage(x1,y1,x2,y2,array of image 2 containting 16-bit RGB565 data of each pixel) 
+ ipsdisp.setCursor(0, 295,4);              //ipsdisp.setCurser(x axis,y axis,text font style i.e 1/2/4/6)
+ ipsdisp.setTextColor(TFT_CYAN,TFT_BLACK); //ipsdisp.setTextColor(text color,text background color)
+ ipsdisp.println(F("IMG TYP:Animated     "));
+ delay(1000); 
+
+ ipsdisp.pushImage(0,0,240,320,image3);    //ipsdisp.pushImage(x1,y1,x2,y2,array of image 3 containting 16-bit RGB565 data of each pixel) 
+ ipsdisp.setCursor(0,295,4);               //ipsdisp.setCurser(x axis,y axis,text font style i.e 1/2/4/6)
+ ipsdisp.setTextColor(TFT_CYAN,TFT_BLACK); //ipsdisp.setTextColor(text color,text background color)
+ ipsdisp.println(F("IMG TYP:HD Photo     "));
+ delay(1000);  
+
 }
